38739e0d516e38d83a48a00a41b07a1886ed8619
Pushkaran <108679715+pushkaran23@users.noreply.github.com>
2025-12-23 13:19:24 +0530

    Add MyTestToken contract with ERC20 implementation

--- contracts/MyTestToken.sol
+++ contracts/MyTestToken.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyTestToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyTestToken", "MTT") {
+        _mint(msg.sender, initialSupply);
+    }
+}
 
